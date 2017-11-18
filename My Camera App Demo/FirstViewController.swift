2ce25fd054fdeb6e21c924fc47fcadc1a26dd70b
Afam Ezechukwu <afamezechukwu@aol.com>
2017-11-18 15:14:03 +0000

    Initial Commit

---
+++ My Camera App Demo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  My Camera App Demo
+//
+//  Created by Afam Ezechukwu on 18/11/2017.
+//  Copyright © 2017 The Gypsy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ My Camera App Demo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  My Camera App Demo
+//
+//  Created by Afam Ezechukwu on 18/11/2017.
+//  Copyright © 2017 The Gypsy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
